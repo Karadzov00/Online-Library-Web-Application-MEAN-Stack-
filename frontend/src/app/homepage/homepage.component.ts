3afbb05f2934ce557aeb68f849a3a6e28f519f7f
Karadzov00 <caka.azdaha@gmail.com>
2022-09-10 03:20:58 +0200

    added carousel on homepage

--- frontend/src/app/homepage/homepage.component.ts
+++ frontend/src/app/homepage/homepage.component.ts
@@ -13,27 +13,11 @@ export class HomepageComponent implements OnInit {
   constructor(private router:Router, private booksService: BooksService ) { }
 
   ngOnInit(): void {
-    this.slides[0] = {
-      id: 0,
-      src: './assets/img/img1.jpg',
-      title: 'First slide',
-      subtitle: 'Nulla vitae elit libero, a pharetra augue mollis interdum.'
-    };
-    this.slides[1] = {
-      id: 1,
-      src: './assets/img/img2.jpg',
-      title: 'Second slide',
-      subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-    }
-    this.slides[2] = {
-      id: 2,
-      src: './assets/img/img3.jpg',
-      title: 'Third slide',
-      subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
-    }
+    
   }
 
-  slides: any[] = new Array(3).fill({id: -1, src: '', title: '', subtitle: ''});
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+
   books: Book[]; 
 
 }
